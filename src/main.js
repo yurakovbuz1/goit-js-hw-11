70a143c937884961fc57620440cd3dfcab6a7e3c
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-04-22 10:53:37 +0200

    gallery implemented

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import { getImages } from "./js/pixabay-api";
 
+const form = document.querySelector(".container")
+const searchTerm = document.querySelector(".searchTerm");
+const gallery = document.querySelector(".gallery");
+form.addEventListener("submit", onFormSubmit);
+const galleryItem = document.querySelector("gallery-item")
+// galleryItem.addEventListener("")
 
+function onFormSubmit(e) {
+    e.preventDefault();
+    getImages(searchTerm.value, gallery);
+}
